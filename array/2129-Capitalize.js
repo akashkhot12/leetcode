afa40e813246f290d60f36f53986a1ffbd4f397c
akashkhot12 <akashkhot03@gmail.com>
2023-07-19 14:32:43 +0530

    adding string

--- array/2129-Capitalize.js
+++ array/2129-Capitalize.js
@@ -3,6 +3,7 @@ function capitalizeTitle(title) {
     for (var _i = 0, sentence_1 = sentence; _i < sentence_1.length; _i++) {
         var sentences = sentence_1[_i];
         var firstLetter = sentences.substring(0, 1);
+        firstLetter = firstLetter.toUpperCase();
         console.log(firstLetter);
     }
     return title;
--- array/2129-Capitalize.ts
+++ array/2129-Capitalize.ts
@@ -1,9 +1,9 @@
 function capitalizeTitle(title: string): any {
 const sentence =title.split(" ")
 for(const sentences of sentence){
-   let firstLetter=sentences.substring(0,1)
+   let firstLetter=sentences.substring(0, 1)
+   firstLetter=firstLetter.toUpperCase()
    console.log(firstLetter);
-   
 }
    return title
 

2b29131469bde0d9970c875e2567b1dba86597f7
akashkhot12 <akashkhot03@gmail.com>
2023-07-19 10:48:59 +0530

    completed

--- array/66-PlusOne.ts
+++ array/66-PlusOne.ts
@@ -1,7 +1,7 @@
-function plusOne(digits: any[]):any{
-    let arr=(parseInt(digits.join("")) + 1).toString()
-    return arr ; 
-     
+function plusOne(digits: number[]):any{
+    let arr=(BigInt(digits.join("")) + BigInt(1)).toString();
+    const arrayOfDigits = Array.from(String(arr));
+    return arrayOfDigits ;    
 };
 
 let a1=plusOne([1,9,9])
